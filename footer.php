ab627c5a1029793bc1e119ad6556de0f8c8b4a3f
Rogerio Augusto Sabino <138535985+Rogerio5@users.noreply.github.com>
2025-07-06 11:48:47 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="text-center mt-4">
+    <p>&copy; <?php echo date("Y"); ?> Descubra Seu Signo. Todos os direitos reservados.</p>
+    <div>
+        <a href="https://www.instagram.com" target="_blank">instagram</a>
+        <a href="https://www.facebbok.com" target="_blank">facebook</a>
+        <a href="https://www.twitter.com" target="_blank">twitter</a>
+    </div>
+
+</footer>
+</body>
+</html>
